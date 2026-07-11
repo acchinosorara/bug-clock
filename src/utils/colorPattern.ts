708f29d2f31340dff45dc9338dfef70d72777af7
gitTest <dq.ta.a1370@gmail.com>
2026-07-11 21:27:36 +0900

    配色をランダムに決定するための拡張

---
+++ src/utils/colorPattern.ts
@@ -0,0 +1,8 @@
+export const colorPattern = (): string[][] => {
+    const white = 'white'
+    const colors = [
+        ['orange', 'blue', white],
+        ['pink', 'green', white]
+    ]
+    return colors
+}
