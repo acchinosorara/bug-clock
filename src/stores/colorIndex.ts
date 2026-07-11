5229a8f373c3c438c3cf92ad352f6e0ef71db908
gitTest <dq.ta.a1370@gmail.com>
2026-07-11 21:36:59 +0900

    配色を決めるため、colorPatternのインデックス番号を出力

---
+++ src/stores/colorIndex.ts
@@ -0,0 +1,9 @@
+export const useColorsIndex = defineStore('colorsIndex', () => {
+    const { randomInt } = random()
+    const colorsLength = colorPattern().length
+    const colorsIndex = ref<number>(randomInt({ min: 0, max: colorsLength }))
+
+    return {
+        colorsIndex
+    }
+})
