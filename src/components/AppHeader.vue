6b051138ced3052b97ee04c72b6b4a59f1c4002d
gitTest <dq.ta.a1370@gmail.com>
2026-07-14 00:34:03 +0900

    headerコンポーネント群の統合先を作成

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Reload from '@/components/header/ReloadButton.vue'
+</script>
+
+<template>
+    <header class="header">
+        <Reload />
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    position: absolute;
+    width: 100%;
+    padding: $border-extrabold;
+    top: 0;
+    left: 0;
+}
+</style>
