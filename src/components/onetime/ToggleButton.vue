bc1d62ff9b1a3c585e4ec9c9ec258ddb4a9d7f45
gitTest <dq.ta.a1370@gmail.com>
2026-08-12 15:49:11 +0900

    アイコンアニメーション追加

--- src/components/onetime/ToggleButton.vue
+++ src/components/onetime/ToggleButton.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
+import { motion } from 'motion-v'
 import AsteriskIcon from '~icons/my-icons/asterisk'
-import { useOnetimeColorsIndexStore } from '@/stores/onetime/onetimeColorsIndex'
 import { useMakeButtonStore } from '@/stores/onetime/makeButton'
 import { useOnetimePopStore } from '@/stores/onetime/onetimePop'
 import { setCommonClass } from '@/utils/commonClass/setCommonClass'
+import type { VariantType, Transition } from 'motion-v'
 
 const onetimePopStore = useOnetimePopStore()
 const { limit } = onetimePopStore
 const { isPopover, timeout } = storeToRefs(onetimePopStore)
 const { makeButton } = storeToRefs(useMakeButtonStore())
 
-// SVGの色
-const { colorClassNames } = setCommonClass()
-const { colorsIndex } = storeToRefs(useColorsIndexStore())
-const { index } = storeToRefs(useOnetimeColorsIndexStore())
-const { randomInt } = random()
-const colorClass = ref<string>('')
+// アイコン
+const { colorClassNames, borderClassNames } = setCommonClass()
+const { colorsIndex, colorIndex } = storeToRefs(useColorsIndexStore())
+const { setColorIndex } = useColorsIndexStore()
+const colorClasses = ref<string[]>([])
+
+const spinAnimate: VariantType = {
+    rotate: [0, 0, 180, 180, 360, 360],
+    scale: [1, 1.25, 1.25, 1.25, 1.25, 1]
+}
+const spinTransition: Transition = {
+    duration: 3,
+    delay: 4,
+    times: [0, 0.1, 0.3, 0.75, 0.9, 1],
+    repeat: Infinity,
+    repeatDelay: 4
+}
+const iconAnimate = computed<VariantType>(() => (isPopover.value ? {} : spinAnimate))
 
 const popToggle = (): void => {
     clearTimeout(timeout.value)
@@ -31,17 +44,19 @@ const popToggle = (): void => {
     }, limit)
 }
 
-const setColorClass = (): void => {
-    const length = colorPattern().length
-    index.value = randomInt({ min: 0, max: length })
-    colorClass.value = colorClassNames[colorsIndex.value]?.[index.value] ?? ''
+const setClass = (): void => {
+    setColorIndex()
+    colorClasses.value = [
+        colorClassNames[colorsIndex.value]?.[colorIndex.value] ?? '',
+        borderClassNames[colorsIndex.value]?.[colorIndex.value] ?? ''
+    ]
 }
 
 watch(isPopover, (val) => {
     if (val) {
-        setColorClass()
+        setClass()
     } else {
-        colorClass.value = ''
+        colorClasses.value = []
     }
 })
 </script>
@@ -50,29 +65,42 @@ watch(isPopover, (val) => {
     <button
         :ref="(el) => (makeButton = el as HTMLButtonElement | null)"
         class="make-btn"
-        :class="colorClass"
+        :class="colorClasses"
         @click="popToggle()"
     >
-        <AsteriskIcon />
+        <motion.span :animate="iconAnimate" :transition="spinTransition" class="make-btn-ic">
+            <AsteriskIcon />
+        </motion.span>
     </button>
 </template>
 
 <style lang="scss" scoped>
 .make-btn {
-    @include flex-center;
     @include circle;
+    position: relative;
     width: $make-button-size;
     background-color: $dark200;
-    border: solid 1px $dark300;
-    margin-left: auto;
+    border: solid 1px $dark400;
     color: $light;
+    transition: all $duration;
 
     &.color {
         @include text-colors;
     }
 
-    svg {
+    &.border {
+        @include border-colors;
+    }
+
+    &-ic {
+        @include center;
+        @include flex-center;
         width: calc(100% / 3);
+
+        svg {
+            width: 100%;
+            height: auto;
+        }
     }
 }
 </style>
