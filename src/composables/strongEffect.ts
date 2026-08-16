2e54507a5cc330d359659c19068a8c647330913d
gitTest <dq.ta.a1370@gmail.com>
2026-08-16 17:36:29 +0900

    強いエフェクトをランダム発生させる

---
+++ src/composables/strongEffect.ts
@@ -0,0 +1,43 @@
+import { useUpdateInterval } from '@/composables/updateInterval'
+import type { StrongEffectName } from '@/stores/strongEffect'
+
+interface StrongEffect {
+    isActive: ComputedRef<boolean>
+    endEffect: () => void
+}
+
+let hasTrigger = false
+
+export const useStrongEffect = (name: StrongEffectName): StrongEffect => {
+    const { randomInt } = random()
+    const strongEffectStore = useStrongEffectStore()
+    const { strongEffects } = strongEffectStore
+    const { activeEffect } = storeToRefs(strongEffectStore)
+
+    if (!hasTrigger) {
+        hasTrigger = true
+
+        useUpdateInterval({
+            delay: 4000,
+            countMax: 10,
+            tick: () => {
+                if (activeEffect.value !== null || isTrigger(2)) return
+
+                const index = randomInt({ min: 0, max: strongEffects.length })
+                activeEffect.value = strongEffects[index] ?? null
+            }
+        })
+    }
+
+    const isActive = computed<boolean>(() => activeEffect.value === name)
+
+    // エフェクト発火中の場合のみ解除
+    const endEffect = (): void => {
+        if (isActive.value) activeEffect.value = null
+    }
+
+    return {
+        isActive,
+        endEffect
+    }
+}
---
+++ src/stores/strongEffect.ts
@@ -0,0 +1,12 @@
+export const strongEffects = ['longHands', 'cloneIndex', 'noiseLarge'] as const
+export type StrongEffectName = (typeof strongEffects)[number]
+
+export const useStrongEffectStore = defineStore('strongEffect', () => {
+    // エフェクト発火は1つのみ
+    const activeEffect = ref<StrongEffectName | null>(null)
+
+    return {
+        strongEffects,
+        activeEffect
+    }
+})
