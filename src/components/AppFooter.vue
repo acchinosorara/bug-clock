ed5fb454583e2a77778386cb7ffefd6d341a2e6e
gitTest <dq.ta.a1370@gmail.com>
2026-08-30 13:54:47 +0900

    footerのコンポーネント化

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { initStyle } = useViewStore()
+</script>
+
+<template>
+    <footer :style="initStyle()" class="footer">
+        <small class="copyright mono">&copy;2026 acchinosorara</small>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+    position: absolute;
+    padding-left: $inline;
+    bottom: $block;
+}
+
+.copyright {
+    color: $light400;
+    font-size: 0.75rem;
+    letter-spacing: 0.05em;
+}
+</style>
