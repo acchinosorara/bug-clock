0a04aadf83527e9c05f8417f5132412621921015
gitTest <dq.ta.a1370@gmail.com>
2026-08-30 14:08:23 +0900

    paddingを共通スタイルに変更

--- src/components/onetime/AppOnetime.vue
+++ src/components/onetime/AppOnetime.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import ToggleButton from './ToggleButton.vue'
 import PopDisplay from './PopDisplay.vue'
+const { initStyle } = useViewStore()
 </script>
 
 <template>
-    <div class="make">
+    <div class="make" :style="initStyle()">
         <ToggleButton />
         <PopDisplay />
     </div>
@@ -18,7 +19,7 @@ import PopDisplay from './PopDisplay.vue'
     align-items: flex-end;
     width: 100%;
     height: 100%;
-    padding: $border-extrabold;
+    padding: $block $inline;
     padding-top: 0;
     top: 0;
     left: 0;
